697cb3f0aaa35ef994ee99249e6e207065fb83b1
SeanQuinn <Seanquinn917@gmail.com>
2023-10-15 21:31:45 -0400

    commit tree c6bb4c116260dca8794170bf404a1a6be24cc93a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
@@ -22,5 +24,31 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+
+  delete "/baked_goods/:id" do 
+  baked_good = BakedGood.find(params[:id])
+  baked_good.destroy
+  baked_good.to_json
+end
+
+post '/baked_goods' do
+  baked_good = BakedGood.create(
+    name: params[:name],
+    price: params[:price],
+    bakery_id: params[:bakery_id],
+    created_at: params[:created_at],
+    updated_at: params[:updated_at]
+  )
+  baked_good.to_json
+end
+
+patch '/bakeries/:id' do
+  bakery = Bakery.find(params[:id])
+  # binding.pry
+  bakery.update(
+    name: params[:name]
+    )
+  bakery.to_json
+end
 
 end
